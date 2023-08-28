8e74774b21df0380de83ac70e6e2f4c630ef0cbe
JeonHaesung <eternalasset92@gmail.com>
2023-08-28 14:47:00 +0900

    Backend Project Start

---
+++ server/src/main/java/com/codestates/connectInstructor/ConnectInstructorApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.connectInstructor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectInstructorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectInstructorApplication.class, args);
+	}
+
+}
