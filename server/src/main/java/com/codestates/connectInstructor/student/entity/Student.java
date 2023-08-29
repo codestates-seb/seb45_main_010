a82ae0ede1f4440f402298e75c32651f7ed230ac
9599562 <9599562@gmail.com>
2023-08-29 11:12:58 +0900

    [Feat] add Student.createdAt

--- server/src/main/java/com/codestates/connectInstructor/student/entity/Student.java
+++ server/src/main/java/com/codestates/connectInstructor/student/entity/Student.java
@@ -1,5 +1,6 @@
 package com.codestates.connectInstructor.student.entity;
 
+import com.codestates.connectInstructor.audit.Auditable;
 import com.codestates.connectInstructor.common.MemberStatus;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
-public class Student {
+public class Student extends Auditable {
     //TODO 컬럼 제약 사항(길이 등등)은 프론트와 상의 후 추가
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
