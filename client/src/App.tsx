89c2a16c2ca3102b5829029a421a6266f1cd5d64
ChoSangok <chod1510@naver.com>
2023-08-30 01:39:23 +0900

    [package] add meterial tailwind추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
+import { Badge, Button, Rating } from '@material-tailwind/react';
 const App = () => {
   return (
     <>
-      <h1 className="mt-10 text-3xl font-bold underline bg-red-100">Hello world!dasd</h1>
+      <Badge content="5">
+        <Button className="bg-red-100">Notifications</Button>
+      </Badge>
+      <Rating />;<h1 className="mt-10 text-3xl font-bold underline bg-red-100">Hello world!dasd</h1>
     </>
   );
 };
