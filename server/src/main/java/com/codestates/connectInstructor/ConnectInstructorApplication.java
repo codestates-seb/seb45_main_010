94266c91145e362c56417684262f7792a16a5204
9599562 <9599562@gmail.com>
2023-08-29 13:23:53 +0900

    [Feat] add 학생 회원가입

--- server/src/main/java/com/codestates/connectInstructor/ConnectInstructorApplication.java
+++ server/src/main/java/com/codestates/connectInstructor/ConnectInstructorApplication.java
@@ -2,8 +2,10 @@ package com.codestates.connectInstructor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
+//		(exclude = SecurityAutoConfiguration.class)
 public class ConnectInstructorApplication {
 
 	public static void main(String[] args) {
--- server/src/main/java/com/codestates/connectInstructor/student/repository/StudentRepository.java
+++ server/src/main/java/com/codestates/connectInstructor/student/repository/StudentRepository.java
@@ -3,5 +3,8 @@ package com.codestates.connectInstructor.student.repository;
 import com.codestates.connectInstructor.student.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByEmail(String email);
 }
