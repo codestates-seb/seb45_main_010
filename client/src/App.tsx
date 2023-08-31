e94e32472e50b06f3518c54ac99dbc5de0ff0233
ChoSangok <chod1510@naver.com>
2023-08-31 13:30:59 +0900

    [Feat] add Sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,24 @@
 import Header from 'components/Header/Header';
+import { Sidebar } from 'components/Menu/Sidebar';
+import { useState } from 'react';
 
 const App = () => {
+  const [isMenu, setIsMenu] = useState<boolean>(false);
+
+  const handlerMenu = (): void => {
+    setIsMenu(!isMenu);
+  };
+
   return (
     <main className="h-screen ">
-      <Header />
+      {isMenu && (
+        <div
+          className="absolute top-0 w-[375px] h-screen bg-black bg-opacity-10"
+          onClick={handlerMenu}
+        ></div>
+      )}
+      <Header handlerMenu={handlerMenu} />
+      {isMenu && <Sidebar />}
     </main>
   );
 };
