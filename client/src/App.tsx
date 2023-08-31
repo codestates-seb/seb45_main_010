e88169629cdcc0dcaa4616cfa021d432e45308d2
ChoSangok <chod1510@naver.com>
2023-08-31 13:30:59 +0900

    [Feat] add Sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,24 @@
 import Header from 'components/Header/Header';
-import Profile from 'pages/TeacherProfile/Profile';
+import { Sidebar } from 'components/Menu/Sidebar';
+import { useState } from 'react';
 
 const App = () => {
+  const [isMenu, setIsMenu] = useState<boolean>(false);
+
+  const handlerMenu = (): void => {
+    setIsMenu(!isMenu);
+  };
+
   return (
     <main className="h-screen ">
-      <Header />
-      <Profile />
+      {isMenu && (
+        <div
+          className="absolute top-0 w-[375px] h-screen bg-black bg-opacity-10"
+          onClick={handlerMenu}
+        ></div>
+      )}
+      <Header handlerMenu={handlerMenu} />
+      {isMenu && <Sidebar />}
     </main>
   );
 };
--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,9 +1,13 @@
 import { AiOutlineMenu, AiOutlineSearch } from 'react-icons/ai';
 
-const Header = () => {
+type props = {
+  handlerMenu: () => void;
+};
+
+const Header = ({ handlerMenu }: props) => {
   return (
     <header className="sticky top-0 grid items-center h-12 grid-cols-3 p-1 mb-10 border-b-2 border-b-grey-2">
-      <AiOutlineMenu className="ml-3 text-2xl cursor-pointer text-grey-3 " />
+      <AiOutlineMenu className="ml-3 text-2xl cursor-pointer text-grey-3 " onClick={handlerMenu} />
       <p className="col-span-2">'임시(서비스명)'</p>
       <AiOutlineSearch className="col-end-5 mr-3 text-2xl cursor-pointer text-grey-3 " />
     </header>
