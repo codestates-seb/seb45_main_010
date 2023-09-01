240576bda8820d783d5ebebe048dc42c98b8af13
ChoSangok <chod1510@naver.com>
2023-09-01 16:39:35 +0900

    [Style] modify 폰트바꿈으로 크기변경

--- client/src/components/Menu/Sidebar.tsx
+++ client/src/components/Menu/Sidebar.tsx
@@ -17,13 +17,13 @@ export const Sidebar = ({ handlerMenu }: props) => {
       <Card className="w-[350px] p-1 shadow-xl  top-0 absolute">
         <List className="grid items-center grid-cols-2 p-4 border-b-2 border-grey-3">
           <ListItem className="flex items-center justify-center">
-            <AiOutlineUser className="mr-2 text-2xl" />
+            <AiOutlineUser className="mr-2 text-xl" />
             <Typography className="mt-1" variant="h5" color="blue-gray">
               로그인
             </Typography>
           </ListItem>
           <ListItem className="flex justify-center ">
-            <AiOutlineUserAdd className="mr-2 text-2xl " />
+            <AiOutlineUserAdd className="mr-2 text-xl " />
             <Typography className="mt-1" variant="h5" color="blue-gray">
               회원가입
             </Typography>
