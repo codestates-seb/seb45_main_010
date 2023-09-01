052ef02687d7dccf9e96bbfcd24b09f0bd11056d
ChoSangok <chod1510@naver.com>
2023-09-01 09:48:26 +0900

    [Style] add 배경 추가

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ type props = {
 
 const Header = ({ handlerMenu, handlerSearch }: props) => {
   return (
-    <header className="sticky top-0 grid items-center h-12 grid-cols-3 p-1 mb-10 border-b-2 border-b-grey-2">
+    <header className="sticky top-0 grid items-center h-12 grid-cols-3 p-1 mb-10 bg-white border-b-2  border-b-grey-2">
       <AiOutlineMenu className="ml-3 text-2xl cursor-pointer text-grey-3 " onClick={handlerMenu} />
       <p className="col-span-2">'임시(서비스명)'</p>
       <AiOutlineSearch
