41372905ad396839e7c946d19999dd35c76279fe
jerimy321 <123806723+jerimy321@users.noreply.github.com>
2023-09-01 14:17:48 +0900

    [feat] layout

--- client/src/router/root.tsx
+++ client/src/router/root.tsx
@@ -1,4 +1,5 @@
 import App from 'App';
+import DetailPage from 'pages/DetailPage/DetailPage';
 import ErrorPage from 'pages/ErrorPage/ErrorPage';
 import Profile from 'pages/Profile/Profile';
 import { createBrowserRouter } from 'react-router-dom';
@@ -12,6 +13,10 @@ export const router = createBrowserRouter([
       {
         path: 'profile',
         element: <Profile />,
+      },
+      {
+        path: 'detail',
+        element: <DetailPage />,
       },
     ],
   },
