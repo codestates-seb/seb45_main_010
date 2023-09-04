790785ea5bb471383accca9a6eb67fd337366b5b
JeonHaesung <eternalasset92@gmail.com>
2023-09-04 16:27:26 +0900

    [Feat] add 예외처리추가. GlobalExceptionAdvice

---
+++ server/src/main/java/com/codestates/connectInstructor/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,68 @@
+package com.codestates.connectInstructor.advice;
+
+
+import com.codestates.connectInstructor.exception.BusinessLogicException;
+import com.codestates.connectInstructor.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        final ErrorResponse response = ErrorResponse.of(e.getBindingResult());
+
+        return response;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleConstraintViolationException(
+            ConstraintViolationException e) {
+        final ErrorResponse response = ErrorResponse.of(e.getConstraintViolations());
+
+        return response;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleBusinessLogicException(BusinessLogicException e) {
+        System.out.println(e.getExceptionCode().getCode());
+        System.out.println(e.getMessage());
+
+        // TODO GlobalExceptionAdvice 기능 추가 1
+        return ErrorResponse.of(e.getExceptionCode() );
+    }
+
+    // TODO GlobalExceptionAdvice 기능 추가 2
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public ErrorResponse handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e){
+        final HttpStatus hs = HttpStatus.METHOD_NOT_ALLOWED;
+        System.out.println( hs.value());
+        System.out.println( hs.getReasonPhrase());
+
+        return ErrorResponse.of( hs );
+    }
+
+    // TODO GlobalExceptionAdvice 기능 추가 3
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handelException( NullPointerException e){
+        final HttpStatus hs = HttpStatus.INTERNAL_SERVER_ERROR;
+        System.out.println( hs.value());
+        System.out.println( hs.getReasonPhrase());
+
+        return ErrorResponse.of( hs );
+
+    }
+
+}
