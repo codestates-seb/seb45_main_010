2b997a128bc00388831234d2310ed4bb3d3f0bb0
JeonHaesung <eternalasset92@gmail.com>
2023-09-05 15:53:46 +0900

    [feat] Region Subject Match 엔티티 조금 작성, TeacherRegion, TeacherSubject 엔티티 생성

--- server/src/main/java/com/codestates/connectInstructor/region/entity/Region.java
+++ server/src/main/java/com/codestates/connectInstructor/region/entity/Region.java
@@ -1,4 +1,17 @@
 package com.codestates.connectInstructor.region.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter@Setter@NoArgsConstructor
 public class Region {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false, unique = true)
+    private String area;
 }
