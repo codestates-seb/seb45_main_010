6b446f43d80ae4cfa02eee04a74d3fd5a5d4ede4
ellanelee <ljedoc@naver.com>
2023-09-06 16:23:50 +0900

    Design_Width_Trim_Member

--- client/src/pages/GetPassword.tsx
+++ client/src/pages/GetPassword.tsx
@@ -50,29 +50,29 @@ const GetPassword: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col item-center justify-center m-[12.5px]">
+    <div className="flex flex-col item-center justify-center px-[12.5px]">
       <div className="text-center font-bold text-2xl my-4">비밀번호 찾기</div>
-      <div className="flex flex-col item-center justify-center mx-3 py-3 rounded-lg">
-        <div className="text-sm text-gray-950 px-5 mb-2">가입된 이메일을 입력해주세요</div>
-        <div className="text-sm text-gray-950 px-5">
-          이메일을 통해 비밀번호를 확인하실수 있습니다
+      <div className="flex flex-col item-center justify-center rounded-lg">
+        <div className="flex flex-col my-2">
+          <div className="text-sm text-gray-950">가입된 이메일을 입력해주세요</div>
+          <div className="text-sm text-gray-950">이메일을 통해 비밀번호를 확인하실수 있습니다</div>
         </div>
-        <form className="flex flex-col gap-2 p-4 m-1 rounded-lg" onSubmit={HandleSearch}>
-          <label htmlFor="email" className="text-sm mx-4 mt-5">
+        <form className="flex flex-col gap-2 py-4" onSubmit={HandleSearch}>
+          <label htmlFor="email" className="text-sm">
             이메일 입력
           </label>
           <input
             type="email"
             id="email"
             name="email"
-            className="border text-xs rounded-lg p-3 mx-[12px] h-[50px]"
+            className="border text-sm rounded-lg p-2 h-[50px] mb-10"
             placeholder="이메일을 입력하세요"
             value={passwordkey.email}
             onChange={handleSecret}
           />
           <Button
             type="submit"
-            className="text-white text-xl bg-blue-1 rounded-lg shadow-lg shadow-gray-900/30 p-2 m-3 mt-10 h-[50px] hover:bg-blue-2"
+            className="text-white border-2 border-blue-1 text-xl bg-blue-1 rounded-lg shadow-lg shadow-gray-900/30 p-1 h-[50px] hover:bg-blue-2"
           >
             요청보내기
           </Button>
