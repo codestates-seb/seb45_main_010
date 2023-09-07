442650125e129f008b04a7e2ffe77301e7eec65c
JeonHaesung <eternalasset92@gmail.com>
2023-09-07 13:23:36 +0900

    [feat] add subject CRUD

--- server/src/main/java/com/codestates/connectInstructor/exception/ExceptionCode.java
+++ server/src/main/java/com/codestates/connectInstructor/exception/ExceptionCode.java
@@ -5,7 +5,10 @@ import lombok.Getter;
 public enum ExceptionCode {
     NOT_AUTHORIZED(403, "NOT AUTHORIZED"),
     MEMBER_NOT_FOUND(404, "MEMBER NOT FOUND"),
+    SUBJECT_NOT_FOUND(404, "SUBJECT NOT FOUND"),
+    SUBJECT_EXISTS(409, "SAME SUBJECT NAME ALREADY EXISTS"),
     USED_EMAIL(409,  "USED EMAIL");
+
 
     @Getter
     private int code;
