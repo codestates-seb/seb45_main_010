8f75472af7c2158f92cd98814988c792de08ecbb
ellanelee <ljedoc@naver.com>
2023-09-08 16:03:40 +0900

    FEAT_PrivatePage_Function_InformationShare

--- client/src/pages/Private.tsx
+++ client/src/pages/Private.tsx
@@ -1,42 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Thumbnail from '../assets/모네-수련.jpeg';
 import { AiFillCamera } from 'react-icons/ai';
 import { FiSend } from 'react-icons/fi';
+import { fetchUserDetails } from 'redux/slice/MemberSlice';
+import { useAppDispatch, useAppSelector } from 'hooks/hooks';
 import { CommonUserType } from '../components/Types/Types';
 
 const Private: React.FC = () => {
-  const [userInfo, setUserInfo] = useState<CommonUserType>({
-    ...userDetails,
-  });
-
+  const dispatch = useAppDispatch();
+  const userDetails = useAppSelector((state) => state.member);
   console.log(userDetails);
-  console.log(userInfo);
+
+  // 연동을 구현하면 삭제해야 할 코드, 해당 페이지 로딩시 새로고침으로 초기화되어 삽입한 내용임
+  const LoginInfo: string = 'abcd@gmail.com';
+  useEffect(() => {
+    dispatch(fetchUserDetails(LoginInfo));
+  }, [dispatch]);
+
   // const handleImage = () => {
   //   axios
   //     .patch(`http://localhost:8080/${userInfo.id}`)
   //     .then((res) => seUserInfo(res.data))
   //     .catch((err) => console.log(err));
   // };
-  useEffect(() => {
-    // userDetails가 변경될 때마다 userInfo를 업데이트
-    setUserInfo({
-      ...userDetails,
-    });
-  }, [userDetails]);
+  // useEffect(() => {
+  //   // userDetails가 변경될 때마다 userInfo를 업데이트
+  //   setUserInfo({
+  //     ...userDetails,
+  //   });
+  // }, [userDetails]);
 
   return (
     <div className="flex flex-col justify-center px-[12.5px]">
       <div className="flex mb-2">
-        <div className="flex flex-col item-center justify-center">
-          {userInfo.img ? (
-            <img src={userDetails.img} className="mx-6 rounded-lg h-14 w-14 m-2" />
-          ) : (
-            <img src={Thumbnail} className="mx-6 rounded-lg h-14 w-14 m-2" />
-          )}
-        </div>
-        <div className="flex flex-col item-center justify-end relative">
-          <div className="rounded-full h-7 w-7 bg-gray-100 absolute bottom-0 right-0 opacity-80">
-            <AiFillCamera className="h-5 w-5 absolute bottom-1.5 right-1" />
+        <div className="flex">
+          <div className="flex flex-row justify-center item-center mr-[-30px]">
+            {userDetails.img ? (
+              <img src={userDetails.img} className="mx-6 rounded-lg h-14 w-14 m-2" />
+            ) : (
+              <img src={Thumbnail} className="mx-6 rounded-lg h-14 w-14 m-2" />
+            )}
+          </div>
+          <div className="flex flex-col item-center justify-end">
+            <div className="flex justify-center items-center rounded-full h-7 w-7 bg-gray-100 opacity-80">
+              <AiFillCamera className="h-5 w-5" />
+            </div>
           </div>
         </div>
         <div className="flex flex-col justify-center">
