497986d05929945fd7d64ae3389f759af08fad5c
ellanelee <ljedoc@naver.com>
2023-09-13 16:39:16 +0900

    FEAT_토크 자동로그인

--- client/src/components/Auth/CheckAuth.tsx
+++ client/src/components/Auth/CheckAuth.tsx
@@ -7,6 +7,7 @@ export const checkAuth = () => {
     console.log(decoded);
     const teacher: string = decoded.roles[0];
     const id: number = decoded.id;
+    console.log(id, teacher);
     if (!teacher || !id) {
       alert('보안으로 인해 로그인이 해제되었습니다. 로그인해 주세요');
     }
