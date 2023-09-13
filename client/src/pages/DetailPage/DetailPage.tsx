49b59ce60bf4bb7ae0276619ad8ece68de93c36f
ChoSangok <chod1510@naver.com>
2023-09-13 13:58:56 +0900

    [Feat] add detail api 적용완료

--- client/src/pages/DetailPage/DetailPage.tsx
+++ client/src/pages/DetailPage/DetailPage.tsx
@@ -3,21 +3,27 @@ import RequestBtn from 'components/DetailPage/RequestBtn';
 import TeacherInfo1 from 'components/DetailPage/TeacherInfo1';
 import TeacherInfo2 from 'components/DetailPage/TeacherInfo2';
 import { useAppDispatch, useAppSelector } from 'hooks/hooks';
+import { useEffect, useState } from 'react';
 import { teacherDetail } from 'redux/slice/DetailPageSlice';
 import { getData } from 'redux/thunk/DetailPageThunk';
 
-type props = Pick<DetailType, 'date' | 'introduce' | 'lectureFee' | 'career'>;
-
 const DetailPage = () => {
-  const teacherInfo = useAppSelector(teacherDetail);
+  const selectedTeacherDetail = useAppSelector(teacherDetail);
   const dispatch = useAppDispatch();
+  const [teacherInfo, setTeacherInfo] = useState({});
 
-  // dispatch(getData);
+  useEffect(() => {
+    dispatch(getData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (selectedTeacherDetail.status === 'fulfilled') setTeacherInfo(selectedTeacherDetail.value);
+  }, [selectedTeacherDetail]);
 
   return (
     <article className="w-full px-[7.5px] flex flex-col ">
-      <TeacherInfo1 />
-      <TeacherInfo2 />
+      <TeacherInfo1 teacherInfo={teacherInfo} />
+      <TeacherInfo2 teacherInfo={teacherInfo} />
       <RequestBtn />
     </article>
   );
--- client/src/redux/thunk/DetailPageThunk.tsx
+++ client/src/redux/thunk/DetailPageThunk.tsx
@@ -1,13 +1,10 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { DetailType } from 'Types/Types';
 import axios from 'axios';
+import { URL } from 'configs/Url/config';
 
 export const getData = createAsyncThunk('getData', async () => {
-  const response = await axios.get(`${URL}/regions`, {
-    headers: {
-      Accept: 'application/json',
-    },
-  });
+  const response = await axios.get(`${URL}/teachers/${1}`);
   const data = response.data;
   return data;
 });
