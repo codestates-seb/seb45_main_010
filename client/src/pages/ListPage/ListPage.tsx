7a4c6e0396215511e9ac581e025a029920f5e8e3
ChoSangok <chod1510@naver.com>
2023-09-14 13:13:02 +0900

    [Feat] modify pagination 서버와 연결하여 변경되도록 수정완료

--- client/src/pages/ListPage/ListPage.tsx
+++ client/src/pages/ListPage/ListPage.tsx
@@ -4,36 +4,38 @@ import Pagination from 'components/ListPage/Pagination';
 import { useAppDispatch, useAppSelector } from 'hooks/hooks';
 import { teacherList } from 'redux/slice/ListPageSlice';
 import { getData } from 'redux/thunk/ListPageThunk';
-import { ListPageType } from 'Types/Types';
-import { search } from 'configs/ListPage/config';
+import { ListPageType, SearchType } from 'Types/Types';
 
 const ListPage = () => {
   const dispatch = useAppDispatch();
   const teacher = useAppSelector(teacherList);
   const [cardList, setCardList] = useState<ListPageType[]>([]);
-  const [currentPage, setCurrentPage] = useState<number>(0);
-  const limit: number = 7;
-  const offset = currentPage * limit;
+  const [page, setPage] = useState<number>(0);
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const size: number = 1;
 
   useEffect(() => {
+    const search: SearchType = {
+      teacherName: '',
+      subject: [],
+      regions: [],
+      size: size,
+      page: page + 1,
+    };
     dispatch(getData(search));
-  }, []);
+  }, [page]);
 
   useEffect(() => {
     if (teacher.status === 'fulfilled') {
       setCardList(teacher.value.data);
+      setTotalPages(teacher.value.pageInfo.totalPages);
     }
   }, [teacher]);
 
   return (
     <>
-      <TeacherCard cardList={cardList.slice(offset, offset + limit)} />
-      <Pagination
-        cardList={cardList}
-        limit={limit}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      />
+      <TeacherCard cardList={cardList} />
+      <Pagination page={page} totalPages={totalPages} setPage={setPage} />
     </>
   );
 };
