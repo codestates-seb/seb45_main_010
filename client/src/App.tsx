253b9880612959464055c9487fb56b17bf5cba7b
ChoSangok <chod1510@naver.com>
2023-09-17 17:00:52 +0900

    [Fix] modify sidebar 폴더 위치 오류 발견으로 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Footer } from 'components/Footer/Footer';
 import Header from 'components/Header/Header';
-import { Sidebar } from 'components/Menu/Sidebar';
-import { Search } from 'components/Menu/Sidebar/Search';
+import { Sidebar } from 'components/Menu/Sidebar/Sidebar';
+import { Search } from 'components/Menu/Search/Search';
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
