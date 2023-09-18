a28193bb4ecfb596f62875fac61ab3998f35d6b8
ChoSangok <chod1510@naver.com>
2023-09-19 03:10:47 +0900

    [Feat] add 스케줄 표시 적용

--- client/src/components/DetailPage/RequestBtn.tsx
+++ client/src/components/DetailPage/RequestBtn.tsx
@@ -1,34 +1,23 @@
 import React from 'react';
 import { ReqModal } from 'components/Modal/ReqModal/ReqModal';
+import { DetailType } from 'Types/Types';
 
 type props = {
-  subjectNames: string[];
-  regionsNames: string[];
-  schedules: {
-    date: string[];
-  }[];
-  onLine: boolean;
-  offLine: boolean;
-  lectureFee: string;
+  teacherInfo: DetailType;
 };
 
-const RequestBtn = ({
-  subjectNames,
-  regionsNames,
-  schedules,
-  onLine,
-  offLine,
-  lectureFee,
-}: props) => {
+const RequestBtn = ({ teacherInfo }: props) => {
+  console.log(teacherInfo);
+
   return (
     <div className="h-[50px] rounded-xl flex bottom-24 items-center justify-between fixed w-[360px] bg-[#008774] px-1 z-50">
-      <div className="text-sm text-white">{lectureFee}</div>
+      <div className="text-sm text-white">{teacherInfo.lectureFee}</div>
       <ReqModal
-        subjectNames={subjectNames}
-        regionsNames={regionsNames}
-        schedules={schedules}
-        onLine={onLine}
-        offLine={offLine}
+        subjectNames={teacherInfo.subjects}
+        regionsNames={teacherInfo.regions}
+        schedules={teacherInfo.schedules}
+        onLine={teacherInfo.onLine}
+        offLine={teacherInfo.offLine}
       />
     </div>
   );
