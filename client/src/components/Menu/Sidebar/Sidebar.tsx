81b57b1966b4d623042e56f06753fc30551de8d9
ChoSangok <chod1510@naver.com>
2023-09-19 00:52:58 +0900

    [Style] modify z-index위치 조정 margin 수정

--- client/src/components/Menu/Sidebar/Sidebar.tsx
+++ client/src/components/Menu/Sidebar/Sidebar.tsx
@@ -10,10 +10,10 @@ export const Sidebar = ({ handlerMenu }: props) => {
   return (
     <>
       <div
-        className="absolute top-0 w-full h-full bg-black bg-opacity-10 backdrop-blur-sm"
+        className="absolute top-0 z-[999] w-full h-full bg-black bg-opacity-10 backdrop-blur-sm"
         onClick={handlerMenu}
       ></div>
-      <Card className="w-[350px] p-2 shadow-xl top-3 absolute m-3 z-50">
+      <Card className="w-[350px] p-2 shadow-xl top-3 mt-8 mx-3 absolute z-[999]">
         <SideMembership handlerMenu={handlerMenu} />
         <SideList handlerMenu={handlerMenu} />
       </Card>
