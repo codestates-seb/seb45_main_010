1a1e7d081961d16aed6c0b1da083b37acfcde57f
ChoSangok <chod1510@naver.com>
2023-09-19 17:00:25 +0900

    [Style] modify 필요없는 토큰 제거

--- client/src/redux/thunk/DetailPageThunk.tsx
+++ client/src/redux/thunk/DetailPageThunk.tsx
@@ -1,17 +1,10 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { DetailType } from 'Types/Types';
 import axios from 'axios';
-import { getAccessToken } from 'components/Items/GetAccessToken';
 import { URL } from 'configs/Url/config';
 
-const token = getAccessToken();
-
 export const getData = createAsyncThunk<DetailType, string>('getData', async (id) => {
-  const response = await axios.get(`${URL}/teachers/${id}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  const response = await axios.get(`${URL}/teachers/${id}`);
   const data = response.data;
   return data;
 });
