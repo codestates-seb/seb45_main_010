e22204660c72240ebbb436dd3127edcbee818250
ChoSangok <chod1510@naver.com>
2023-09-22 07:40:09 +0900

    [Fix] modify strictmode 제거

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,9 +7,7 @@ import { Provider } from 'react-redux';
 import { store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
